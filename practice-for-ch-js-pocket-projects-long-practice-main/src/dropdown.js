dc9db7beca59859c7803533323b13a298aa44e59
Shannon Millar <shanenak@gmail.com>
2023-09-13 16:33:24 -0700

    Finish nav dropdown

--- practice-for-ch-js-pocket-projects-long-practice-main/src/dropdown.js
+++ practice-for-ch-js-pocket-projects-long-practice-main/src/dropdown.js
@@ -8,3 +8,46 @@ const dogs =
   "Labrador Retriever": "https://www.akc.org/dog-breeds/labrador-retriever/",
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
+
+const dogLinkCreator = function() {
+  const dogLinks = [];
+  for (const key in dogs) {
+    const a = document.createElement("a")
+    a.innerHTML = key;
+    a.href = dogs[key]
+    const li = document.createElement("li")
+    li.className = 'dog-link'
+    li.setAttribute("hidden", "hidden")
+    li.append(a)
+    dogLinks.push(li)
+  }
+  return dogLinks
+}
+
+const dropdownList = document.querySelector(".drop-down-dog-list")
+const dropdownText = document.querySelector(".drop-down-dog-nav")
+const handleEnter = function() { 
+  Array.from(dropdownList.children).forEach((ele) => {
+    ele.removeAttribute("hidden");
+  })
+}
+
+const handleLeave = function() {
+  Array.from(dropdownList.children).forEach((ele) => {
+    ele.setAttribute("hidden", "hidden");
+  })
+}
+
+const attachDogLinks = function() {
+  const dogLinks = dogLinkCreator()
+  dropdownText.addEventListener("mouseenter", handleEnter)
+  dropdownText.addEventListener("mouseleave", handleLeave)
+
+  dogLinks.forEach((ele) => {
+    dropdownList.appendChild(ele);
+  })
+}
+
+
+
+export default attachDogLinks;
--- practice-for-ch-js-pocket-projects-long-practice-main/src/index.js
+++ practice-for-ch-js-pocket-projects-long-practice-main/src/index.js
@@ -1,5 +1,6 @@
 import * as warmUp from "./warmup";
 import Clock from "./clock";
+import attachDogLinks from "./dropdown";
 
 const clock = new Clock();
 
@@ -7,3 +8,5 @@ const clockEle = document.querySelector("#clock");
 setInterval(() => {
     warmUp.htmlGenerator(clock.getTime(), clockEle)
 }, 1000);
+
+attachDogLinks()
